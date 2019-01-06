15507493ea49bc2e6774dc63ff9ed7a849555ebb
Tristan Perera <tristan.e.perera@gmail.com>
2019-01-05 20:59:24 -0800

    completed models

--- controllers/alerts_controller.js
+++ controllers/alerts_controller.js
@@ -1,3 +1,26 @@
-exports.index = function(req, res){
-  res.render("index");
-}
+var db  = require('../models');
+
+exports.index = function(req, res) {
+  db.Alert.findAll({
+    where: {
+      UserId: req.user.id
+    }
+  }).then(function(dbAlert) {
+    console.log(dbAlert);
+    res.render('alerts/alerts', {
+      layout: 'main-alerts',
+      alert: dbAlert
+    });
+  });
+};
+
+// exports.createAlert = function(req, res) {
+
+//   console.log(req.user);
+//   // Add id from User onto req.body
+//   req.body.UserId = req.user.id;
+
+//   db.Trip.create(req.body).then(function(dbPost) {
+//     res.json(dbPost);
+//   });
+// };
